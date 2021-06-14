1287d8de15085b440b006a82328e074f58335a32
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-06-14 23:40:44 +0200

    de

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ multipart_form_data = {
     'jarfile': ('i5.las2peer.services.las2peerakg-1.0.5.jar', open('i5.las2peer.services.las2peerakg-1.0.5.jar', 'rb')),
     'supplement': '{"class":"akgService","name":"akgService","description":"","vcsUrl":"","frontendUrl":"/akg"}'
 }
-cookies = {'sessionid': 'c35m8r7hut3h3kgc4vs4v4paseirpercrij560h20id774ob645m'}
+cookies = {'sessionid': 'fip1udugf808ab0ematoi620hhdn062aebprahjshqia3tv0b997'}
 
 response = requests.post('https://cae-dev.tech4comp.dbis.rwth-aachen.de/ba-erdzan/las2peer/services/upload', files=multipart_form_data, cookies=cookies)
 
