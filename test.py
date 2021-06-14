211648d15067f12640c2e9604c178bdb584541a1
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-06-14 23:40:18 +0200

    de

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ multipart_form_data = {
 }
 cookies = {'sessionid': 'c35m8r7hut3h3kgc4vs4v4paseirpercrij560h20id774ob645m'}
 
-response = requests.post('https://las2peer.tech4comp.dbis.rwth-aachen.de/las2peer/services/upload', files=multipart_form_data, cookies=cookies)
+response = requests.post('https://cae-dev.tech4comp.dbis.rwth-aachen.de/ba-erdzan/las2peer/services/upload', files=multipart_form_data, cookies=cookies)
 
 print(response.content)
