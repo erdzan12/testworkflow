0a73f3d7eff2e197b3b2c48e532f6793d6ec9690
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-06-22 19:44:43 +0200

    cd

--- test.py
+++ test.py
@@ -2,11 +2,12 @@ import requests
 import sys
 
 multipart_form_data = {
-    'jarfile': ('i5.las2peer.services.las2peerakg-1.0.5.jar', open('i5.las2peer.services.las2peerakg-1.0.5.jar', 'rb')),
+    'jarfile': ('i5.las2peer.services.las2peerakg-1.0.5.jar', open('export/jars/i5.las2peer.services.las2peerakg-1.0.0.jar', 'rb')),
     'supplement': '{"class":"akgService","name":"akgService","description":"","vcsUrl":"","frontendUrl":"/akg"}'
 }
 cookies = {'sessionid': sys.argv[1]}
 
-response = requests.post('https://cae-dev.tech4comp.dbis.rwth-aachen.de/ba-erdzan/las2peer/services/upload', files=multipart_form_data, cookies=cookies)
+response = requests.post('https://cae-dev.tech4comp.dbis.rwth-aachen.de/ba-erdzan/las2peer/services/upload',
+                         files=multipart_form_data, cookies=cookies)
 
 print(response.content)
